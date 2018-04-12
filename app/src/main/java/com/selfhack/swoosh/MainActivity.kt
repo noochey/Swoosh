287ef1ecba9577c74b4f05c76d7f23d24f5c9c51
nibreining <nibreining@dagdasolutions.de>
2018-04-12 20:17:06 +0200

    first code

--- app/src/main/java/com/selfhack/swoosh/MainActivity.kt
+++ app/src/main/java/com/selfhack/swoosh/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.selfhack.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonGetStarted.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
